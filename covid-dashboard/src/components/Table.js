cdde943f9d4fb4940bbc0b0c5558f6eea1ecb803
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-12-17 23:26:09 +0300

    fix: correct text in table header

--- covid-dashboard/src/components/Table.js
+++ covid-dashboard/src/components/Table.js
@@ -2,25 +2,25 @@ class Table {
   constructor(summary) {
     this.summary = summary;
     this.period = 'Total';
-    this.relativeAbsoluteValue = 'absolute value';
+    this.relativeAbsoluteValue = 'absolute values';
   }
 
   getAnotherRelativeAbsoluteValue() {
-    if (this.relativeAbsoluteValue === 'absolute value') {
-      return 'relative value';
+    if (this.relativeAbsoluteValue === 'absolute values') {
+      return 'relative values';
     }
-    return 'absolute value';
+    return 'absolute values';
   }
 
   setAnotherRelativeAbsoluteValue() {
-    this.relativeAbsoluteValue = this.relativeAbsoluteValue === 'absolute value' ? 'relative value' : 'absolute value';
+    this.relativeAbsoluteValue = this.relativeAbsoluteValue === 'absolute values' ? 'relative values' : 'absolute values';
   }
 
   getAnotherPeriod() {
     if (this.period === 'Total') {
-      return 'new';
+      return 'new day';
     }
-    return 'total';
+    return 'total period';
   }
 
   setAnotherPeriod() {
@@ -34,7 +34,7 @@ class Table {
   }
 
   setTableData() {
-    this.table.innerHTML = `<span>World for ${this.period} and with ${this.relativeAbsoluteValue}</span>
+    this.table.innerHTML = `<span>World statistic for total period with ${this.relativeAbsoluteValue}</span>
         <span>Confirmed: ${this.summary.TotalConfirmed}</span>
         <span>Deaths: ${this.summary.TotalDeaths}</span>
         <span>Recovered: ${this.summary.TotalRecovered}</span>`;
@@ -43,18 +43,19 @@ class Table {
 
   renderTable(countryData = this.summary) {
     const worldPopulation = 7827000000;
+    const currentPeriod = this.period === 'Total' ? `${this.period.toLowerCase()} period` : `${this.period.toLowerCase()} day`;
     const countryName = countryData.Country ? countryData.Country : 'World';
     const population = countryData.population ? countryData.population : worldPopulation;
     if (this.relativeAbsoluteValue.includes('relative')) {
       const relativeConfirmed = Math.round((countryData[`${this.period}Confirmed`] * 100000) / population);
       const relativeDeaths = Math.round((countryData[`${this.period}Deaths`] * 100000) / population);
       const relativeRecovered = Math.round((countryData[`${this.period}Recovered`] * 100000) / population);
-      this.table.innerHTML = `<span>${countryName} for ${this.period.toLowerCase()} and with ${this.relativeAbsoluteValue}</span>
+      this.table.innerHTML = `<span>${countryName} statistic for ${currentPeriod} with ${this.relativeAbsoluteValue}</span>
       <span>Confirmed: ${relativeConfirmed}</span>
       <span>Deaths: ${relativeDeaths}</span>
       <span>Recovered: ${relativeRecovered}</span>`;
     } else {
-      this.table.innerHTML = `<span>${countryName} for ${this.period.toLowerCase()} and with ${this.relativeAbsoluteValue}</span>
+      this.table.innerHTML = `<span>${countryName} statistic for ${currentPeriod} with ${this.relativeAbsoluteValue}</span>
       <span>Confirmed: ${countryData[`${this.period}Confirmed`]}</span>
       <span>Deaths: ${countryData[`${this.period}Deaths`]}</span>
       <span>Recovered: ${countryData[`${this.period}Recovered`]}</span>`;
