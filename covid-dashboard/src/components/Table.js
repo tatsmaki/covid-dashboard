904adfda25ebff7f21528e6b5caa6c07bb59aba6
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-12-17 20:27:06 +0300

    feat: rebase develop

--- covid-dashboard/src/components/Table.js
+++ covid-dashboard/src/components/Table.js
@@ -1,45 +1,47 @@
 class Table {
-    constructor(summary) {
-        this.summary = summary;
-        this.period = 'full year';
-        this.relativeAbsoluteValue = 'absolute value';
-        console.log('create table')
-    }
+  constructor(summary) {
+    this.summary = summary;
+    this.period = 'full year';
+    this.relativeAbsoluteValue = 'absolute value';
+    console.log('create table')
+  }
 
-    getAnotherRelativeAbsoluteValue() {
-        if (this.relativeAbsoluteValue === 'absolute value') {
-            return 'relative value';
-        }
-        return 'absolute value';
+  getAnotherRelativeAbsoluteValue() {
+    if (this.relativeAbsoluteValue === 'absolute value') {
+      return 'relative value';
     }
-    setAnotherRelativeAbsoluteValue() {
-        this.relativeAbsoluteValue = this.relativeAbsoluteValue === 'absolute value' ? 'relative value' : 'absolute value';
-    }
+    return 'absolute value';
+  }
 
-    getAnotherPeriod() {
-        if (this.period === 'full year') {
-            return 'last day';
-        }
-        return 'full year';
-    }
-    setAnotherPeriod() {
-        this.period = this.period === 'full year' ? 'last day' : 'full year';
-    }
+  setAnotherRelativeAbsoluteValue() {
+    this.relativeAbsoluteValue = this.relativeAbsoluteValue === 'absolute value' ? 'relative value' : 'absolute value';
+  }
 
-    displayTable() {
-        this.table = document.createElement('div');
-        this.table.classList.add('table');
-        return this.setTableData();
+  getAnotherPeriod() {
+    if (this.period === 'full year') {
+      return 'last day';
     }
-    
-    setTableData(data = this.summary, country = 'World') {
-        this.table.innerHTML =
-            `<span>${country} for ${this.period} and with ${this.relativeAbsoluteValue}</span>
+    return 'full year';
+  }
+
+  setAnotherPeriod() {
+    this.period = this.period === 'full year' ? 'last day' : 'full year';
+  }
+
+  displayTable() {
+    this.table = document.createElement('div');
+    this.table.classList.add('table');
+    return this.setTableData();
+  }
+
+  setTableData(data = this.summary, country = 'World') {
+    this.table.innerHTML =
+      `<span>${country} for ${this.period} and with ${this.relativeAbsoluteValue}</span>
         <span>Confirmed: ${data.TotalConfirmed}</span>
         <span>Deaths: ${data.TotalDeaths}</span>
         <span>Recovered: ${data.TotalRecovered}</span>`
-        return this.table;
-    }
+    return this.table;
+  }
 
   renderTable(countryData) {
     this.table.innerHTML = `<span>${countryData.Country}</span>
